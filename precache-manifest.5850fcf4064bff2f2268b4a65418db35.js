d1869b81d605bf6728626601ad2c93d058b33220
MrSourphiz <verymadball@gmail.com>
2020-04-11 03:20:39 +0800

    Updates

---
+++ precache-manifest.5850fcf4064bff2f2268b4a65418db35.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b31c05ea9627982c6b6ca9a9a88e2de",
+    "url": "/to-ocrv/index.html"
+  },
+  {
+    "revision": "9975d5bc7fa22101ef46",
+    "url": "/to-ocrv/static/css/main.0358b3b0.chunk.css"
+  },
+  {
+    "revision": "991fce55ff864f25b041",
+    "url": "/to-ocrv/static/js/2.a94fa4cd.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/to-ocrv/static/js/2.a94fa4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9975d5bc7fa22101ef46",
+    "url": "/to-ocrv/static/js/main.5e0ecb3c.chunk.js"
+  },
+  {
+    "revision": "b3c9cf17920a1a26f3c4",
+    "url": "/to-ocrv/static/js/runtime-main.14e45918.js"
+  },
+  {
+    "revision": "aa73561445be4ab2be6c52de6c5b46d3",
+    "url": "/to-ocrv/static/media/back_logo.aa735614.svg"
+  },
+  {
+    "revision": "0187055ff8fef5ef31e78f693d4abdf5",
+    "url": "/to-ocrv/static/media/gh_logo.0187055f.svg"
+  },
+  {
+    "revision": "4af4bb12d3dc931ff496dd983d545b61",
+    "url": "/to-ocrv/static/media/sf_logo.4af4bb12.svg"
+  },
+  {
+    "revision": "09e204efd6735a1efa594bedb1e746c6",
+    "url": "/to-ocrv/static/media/star.09e204ef.svg"
+  }
+]);
